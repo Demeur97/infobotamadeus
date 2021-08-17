1587c372d13977166662995871165a9087ec02d5
Demeur97 <88535484+Demeur97@users.noreply.github.com>
2021-08-18 00:40:43 +0300

    Delete random_photo.py

--- random_photo.py
+++
@@ -1,9 +0,0 @@
-ph1 = open ('random/1.jpg', 'rb')
-ph2 = open ('random/2.jpg', 'rb')
-ph3 = open ('random/3.jpg', 'rb')
-ph4 = open ('random/4.jpg', 'rb')
-ph5 = open ('random/5.jpg', 'rb')
-ph6 = open ('random/6.jpg', 'rb')
-ph7 = open ('random/7.jpg', 'rb')
-ph8 = open ('random/8.jpg', 'rb')
-random_photo = [ph1, ph2, ph3, ph4, ph5,  ph6, ph7, ph8]
